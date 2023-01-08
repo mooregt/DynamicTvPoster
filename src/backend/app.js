ee23a1f9cab80588c7f0ff8b6430ff5de8d9ed61
George Moore <g.t.moore@lancaster.ac.uk>
2023-01-08 12:34:29 +0000

    Added img element for poster

--- src/backend/app.js
+++ src/backend/app.js
@@ -4,11 +4,12 @@ const shows = require('./modules/shows.js')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
+app.get('/first', (req, res) => {
   let showList = shows.getTvTimeData()
 
   showList.then(function(result) {
     console.info(result)
+    res.set('Access-Control-Allow-Origin', 'http://localhost:3001')
     res.send(result[0])
   })  
 })
--- src/frontend/dynamic-tv-poster/src/App.js
+++ src/frontend/dynamic-tv-poster/src/App.js
@@ -1,25 +1,55 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from 'react';
+
+function MyComponent() {
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [items, setItems] = useState([]);
+
+  // Note: the empty deps array [] means
+  // this useEffect will run once
+  // similar to componentDidMount()
+  useEffect(() => {
+    fetch("http://localhost:3000/first")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setItems(result);
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      )
+  }, [])
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Loading...</div>;
+  } else {
+    return (
+      <img src={items.img}/>
+    );
+  }
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <MyComponent />
       </header>
     </div>
   );
 }
 
+// setInterval(() => {
+//   MyComponent()
+// }, 1000);
 export default App;
